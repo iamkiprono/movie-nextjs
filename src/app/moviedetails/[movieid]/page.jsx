dee02a339587e052cb07d975ebee062b1ff88b2c
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-05-18 11:24:20 +0300

    Update Movie details

--- src/app/moviedetails/[movieid]/page.jsx
+++ src/app/moviedetails/[movieid]/page.jsx
@@ -7,6 +7,11 @@ const page = async ({ params }) => {
     `https://api.themoviedb.org/3/movie/${movieid}?api_key=aeeb61963da597e184eba3a9b3377487`
   );
   const movie = await singleMovie.json();
+
+  const calcRatings = (rate) => {
+    const rating = (rate / 10) * 100;
+    return rating;
+  };
   return (
     <div
       style={{
@@ -14,6 +19,7 @@ const page = async ({ params }) => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
+      className="text-white"
     >
       <p className="mt-6 text-center text-xl font-bold">Movie Details</p>
       <div className="p-6">
@@ -26,12 +32,10 @@ const page = async ({ params }) => {
           />
           <h4 className="my-4 text-3xl font-bold">{movie.original_title}</h4>
           <h4 className="mb-4">{movie.overview}</h4>
-          <Image
-            src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-            width={500}
-            height={500}
-            alt={movie.original_title}
-          />
+          <p className="font-bold">
+            Release Year: {new Date(movie.release_date).getFullYear()}
+          </p>
+          <p>Rating: {Math.floor(calcRatings(movie.vote_average))}%</p>
         </div>
       </div>
     </div>
